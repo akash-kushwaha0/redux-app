9af8cd0a5d2ba8dce42b79a98dbfcb43fe45f47e
akash kushwaha <akashkushwaha76986@gmail.com>
2023-03-30 10:20:33 +0530

    Redux

---
+++ src/Shop.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const Shop = () => {
+  return (
+    <div>
+      <h2>Buy Adidas Shoes 50Rs</h2>
+        <button type="button" class="btn btn-primary">-</button>
+        Add to cart
+        <button type="button" class="btn btn-primary">+</button>
+    </div>
+  )
+}
+
+export default Shop
+                 
